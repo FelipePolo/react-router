722d68535c74c06915235e38327b7c0a5b1199ed
KanekiPrograming <54086856+KanekiPrograming@users.noreply.github.com>
2020-06-23 12:14:53 -0500

    firstcommit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+function Index() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+ReactDom.render(<Index></Index>,document.getElementById("root"))
